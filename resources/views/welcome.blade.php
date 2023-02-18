a952ea9c802e245ca57d7be32a87c6152ad57c38
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2023-02-18 10:37:07 +0500

    practicing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,44 +2,72 @@
 @section('title','HomePage')
 
 @section('content')
-<div class="container-fluid p-0">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="jumbotron jumbotron-fluid bg-primary text-white">
-                <div class="container">
-                    <h1 class="display-4">Welcome to My Website!</h1>
-                    <p class="lead">This is a website landing page built using Bootstrap classes in a Laravel .blade
-                        file syntax.</p>
-                    <hr class="my-4">
-                    <p>Learn more about what we do and how we can help you.</p>
-                    <a class="btn btn-light btn-lg" href="#" role="button">Learn More</a>
+<!doctype html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <title>My Website</title>
+        <!-- Include Bootstrap CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+    </head>
+
+    <body>
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="#">My Website</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <!-- Registration form and image -->
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-md-6">
+                    <h3>Register</h3>
+                    <form>
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" class="form-control" id="name" placeholder="Enter your name">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" class="form-control" id="email" placeholder="Enter your email">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" class="form-control" id="password" placeholder="Enter your password">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+                <div class="col-md-6">
+                    <img src="{{ asset('images/my-image.jpg') }}" alt="My Image" class="img-fluid">
                 </div>
             </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-md-6">
-            <div class="card mb-3">
-                <img class="card-img-top" src="https://picsum.photos/500/300?random=1" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card Title 1</h5>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
-                        Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div class="card mb-3">
-                <img class="card-img-top" src="https://picsum.photos/500/300?random=2" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card Title 2</h5>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
-                        Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+
+        <!-- Include jQuery and Bootstrap JS -->
+        <script src="{{ asset('js/jquery.min.js') }}"></script>
+        <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    </body>
+
+</html>
 @endsection
