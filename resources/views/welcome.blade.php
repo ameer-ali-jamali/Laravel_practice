c08f8920a3fe44085b714e30ebe581a72f46dfac
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2023-02-18 10:24:49 +0500

    creating webiste home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,26 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('main')
+@section('title','HomePage')
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
-    </head>
-
-    <?php
-
-
-$a = MySingleton::getInstance();
-$a->setValue(1);
-echo $a->getValue(); // outputs 1
-
-$b = MySingleton::getInstance();
-echo $b->getValue(); // outputs 1
-
-$b->setValue(2);
-echo $a->getValue(); // outputs 2
-
-?>
-
-</html>
+@section('content')
+<div class="container-fluid p-0">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="jumbotron jumbotron-fluid bg-primary text-white">
+                <div class="container">
+                    <h1 class="display-4">Welcome to My Website!</h1>
+                    <p class="lead">This is a website landing page built using Bootstrap classes in a Laravel .blade
+                        file syntax.</p>
+                    <hr class="my-4">
+                    <p>Learn more about what we do and how we can help you.</p>
+                    <a class="btn btn-light btn-lg" href="#" role="button">Learn More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card mb-3">
+                <img class="card-img-top" src="https://picsum.photos/500/300?random=1" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Card Title 1</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
+                        Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card mb-3">
+                <img class="card-img-top" src="https://picsum.photos/500/300?random=2" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Card Title 2</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
+                        Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [controller11::class, 'store']);
+Route::get('/', function () {
+    return view('welcome');
+});
