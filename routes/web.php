586fc11c2ec49326c6c44b2264a90c39efd338ed
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2023-02-25 21:12:48 +0500

    include routes

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
-Route::get('/about', function () {
+Route::get('about', function () {
     return view('about');
 });
-Route::get('/contact', function () {
+Route::get('contact', function () {
     return view('contact');
 });
