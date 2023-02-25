d1131e5bd4518ed5589f28315c5c1e7290c4e662
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2023-02-25 21:07:34 +0500

    changed navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,74 +2,39 @@
 @section('title','HomePage')
 
 @section('content')
-<!doctype html>
-<html lang="en">
-
-    <head>
-        <meta charset="UTF-8">
-        <title>@yield('title')</title>
-        <!-- Include Bootstrap CSS -->
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-    </head>
-
-    <body>
-        <!-- Navbar -->
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container">
-                <a class="navbar-brand" href="#">IG Students</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="/home">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/about">About</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/contact">Contact</a>
-                        </li>
-                    </ul>
+<!-- Registration form and image -->
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-6">
+            <h3>Register</h3>
+            <form>
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" class="form-control" id="name" placeholder="Enter your name">
                 </div>
-            </div>
-        </nav>
-
-        <!-- Registration form and image -->
-        <div class="container mt-5">
-            <div class="row">
-                <div class="col-md-6">
-                    <h3>Register</h3>
-                    <form>
-                        <div class="form-group">
-                            <label for="name">Name</label>
-                            <input type="text" class="form-control" id="name" placeholder="Enter your name">
-                        </div>
-                        <div class="form-group">
-                            <label for="email">Email</label>
-                            <input type="email" class="form-control" id="email" placeholder="Enter your email">
-                        </div>
-                        <div class="form-group">
-                            <label for="password">Password</label>
-                            <input type="password" class="form-control" id="password" placeholder="Enter your password">
-                        </div>
-                        <button type="submit" onclick="submitForm()" class="btn btn-primary mt-4">Submit</button>
-                    </form>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control" id="email" placeholder="Enter your email">
                 </div>
-                <div class="col-md-6">
-                    <img src="https://pixlr.com/images/index/remove-bg.webp" alt="My Image" class="img-fluid">
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" id="password" placeholder="Enter your password">
                 </div>
-            </div>
-
+                <button type="submit" onclick="submitForm()" class="btn btn-primary mt-4">Submit</button>
+            </form>
         </div>
+        <div class="col-md-6">
+            <img src="https://pixlr.com/images/index/remove-bg.webp" alt="My Image" class="img-fluid">
+        </div>
+    </div>
 
-        <!-- Include jQuery and Bootstrap JS -->
-        <script src="{{ asset('js/jquery.min.js') }}"></script>
-        <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-        <script>
-            function submitForm(formId) {
+</div>
+
+<!-- Include jQuery and Bootstrap JS -->
+<script src="{{ asset('js/jquery.min.js') }}"></script>
+<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+<script>
+    function submitForm(formId) {
             // Get the form element
             // var form = document.getElementById(formId);
 
@@ -82,8 +47,6 @@
             // Display the alert
             alert('Form submitted successfully!');
             }
-        </script>
-    </body>
+</script>
 
-</html>
 @endsection
